5637e62a65b6de93859af96c9569b5297f83e518
Minitha Sai Penumachha <minipenumatcha8@gmail.com>
2022-11-30 16:14:25 -0600

    Changes made to the files

--- models/tennis.js
+++ models/tennis.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose")
 const tennisSchema = mongoose.Schema({
-Player_Name: String,
+Player_Name: {
+    type: String,
+    required: [true, "Name required"]
+},
 Player_Age: Number,
-No_Of_Matches_Played: Number
+No_Of_Matches_Played: {
+    type: Number,
+    min: [1, "Min value is 1"],
+    max: [50, "Max value is 400"]
+}
 })
 module.exports = mongoose.model("tennis",
 tennisSchema)
--- routes/tennis.js
+++ routes/tennis.js
@@ -2,6 +2,14 @@ var express = require('express');
 const tennis_controlers= require('../controllers/tennis'); 
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET home page. */
 
 // router.get('/', function (req, res, next) {
@@ -9,7 +17,7 @@ var router = express.Router();
 // });
 router.get('/', tennis_controlers.tennis_view_all_Page ); 
 router.get('/detail', tennis_controlers.tennis_view_one_Page);
-router.get('/create', tennis_controlers.tennis_create_Page);
-router.get('/update', tennis_controlers.tennis_update_Page);
-router.get('/delete', tennis_controlers.tennis_delete_Page);
+router.get('/create', secured, tennis_controlers.tennis_create_Page);
+router.get('/update', secured, tennis_controlers.tennis_update_Page);
+router.get('/delete', secured, tennis_controlers.tennis_delete_Page);
 module.exports = router;
